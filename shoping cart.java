d1313302a752e00ec66587eac330b79cfa41fde1
Tejashwini120 <151989038+Tejashwini120@users.noreply.github.com>
2023-11-25 14:23:39 +0530

    Add files via upload

---
+++ shoping cart.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.lang.*;
+public class Shoppingcart {
+	private static final int GST = 0;
+	private static final Object[] MAX = null;
+
+	public static void main(int gst2, Object sum) {
+		
+		System.out.println("there are four products,price,gst,quantity");
+		System.out.println("leather wallet,1100,18,1");
+		System.out.println("umbrella,900,12,4");
+		System.out.println("cigarette,200,28,3");
+		System.out.println("honey,100,0,2");
+		Object price = GST%5;
+		price = GST%100;
+		for(int i=0;i>500;i++)
+		if(GST<0)
+		System.out.println("the max gst of the product");
+		Object total = sum;
+		Create Table
+		Row = Product
+                Row = Price
+                Row=GST
+                Row = quality
+                Price = 1100
+                Price = 900
+                Price 200
+                Price =100
+		GST =leather"110";
+		GST =umbrella"900";
+	        GST =cigarette"200";
+		GST =honey"0";
+		quantity=1
+		 quantity=4
+	         quantity=3
+	         quantity=2
+                 price = GST%100;
+		for(int i=0;i>500;i++)
+		if(GST<0)
+		System.out.println("the max gst of the product");
+		Object total = sum;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	}
